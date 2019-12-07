445199722c1aed85f4d408847bd41aa748d7e2c2
Maíra Preto <mairapreto@me.com>
2019-12-06 22:06:53 -0300

    remover arquivos

--- Pods/Target Support Files/Pods-AppReceitaReceberRestaurante-AppReceitaReceberRestauranteUITests/Pods-AppReceitaReceberRestaurante-AppReceitaReceberRestauranteUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AppReceitaReceberRestaurante_AppReceitaReceberRestauranteUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AppReceitaReceberRestaurante_AppReceitaReceberRestauranteUITests
-@end
